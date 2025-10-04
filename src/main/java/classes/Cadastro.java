cfb699d4dcea2128d1564835bb455eceab6b807e
clayton-nunciatto <clayton.nunciatto@gmail.com>
2025-10-04 15:24:17 -0300

    encapsulamento

---
+++ src/main/java/classes/Cadastro.java
@@ -0,0 +1,18 @@
+package classes;
+
+public class Cadastro {
+    private String nome;
+    private int rg;
+    private String cpf;
+
+    public  void setNome(String nome) {this.nome = nome;}
+    public  String getNome() {return nome;}
+
+    public  void setRg(int rg) {this.rg = rg;}
+    public  int getRg() {return rg;}
+
+    public  void setCpf(String cpf) {this.cpf = cpf;}
+    public  String getCpf() {return cpf;}
+
+}
+
